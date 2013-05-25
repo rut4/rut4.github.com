2ad8ce97312a7fc10cea70907de90cc535d010a1
Eduard <rut4ik@gmail.com>
2013-05-25 13:00:43 +0400

    add comments

--- homework/hw_1/js/hw_1_1.js
+++ homework/hw_1/js/hw_1_1.js
@@ -3,4 +3,28 @@ $(function () {
 	if (touch) {
 		$(".profile").addClass("no-touch");
 	}
+	$("#comments")
+	.on("focus", "#commentsText", IncreaseHeight)
+	.on("blur", "#commentsText", DecreaseHeight)
+	.on("click", "#addAnswer", AddAnswer);
+
 })
+
+function IncreaseHeight() {
+	$(this).animate({"height": $(this).height()*2 + "px"});
+}
+
+function DecreaseHeight() {
+	$(this).animate({"height": $(this).height()/2 + "px"});
+}
+
+function AddAnswer(event) {
+	event.preventDefault();
+	$("#oldComments").find(".form_comment").animate({"height": 0}, function () { $(this).remove(); });
+	var formComment = $("#newComment").children(".form_comment");
+	var height = formComment.height();
+	formComment = formComment.clone(true, true);
+	formComment.css("height", 0);
+	$(this).after(formComment);
+	formComment.animate({"height": height + "px"});
+}
