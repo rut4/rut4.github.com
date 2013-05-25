de7e4f5ee5d7fd70943f7ebaa3b2dcf44727905f
Eduard <rut4ik@gmail.com>
2013-05-25 15:02:52 +0400

    comment now adds

--- homework/hw_1/js/hw_1_1.js
+++ homework/hw_1/js/hw_1_1.js
@@ -3,8 +3,9 @@ $(function () {
 	if (touch) {
 		$(".profile").addClass("no-touch");
 	}
-	$("#comments").on("click", "#addAnswer", AddAnswer);
-
+	$("#comments")
+	.on("click", "#addAnswer", AddAnswer)
+	.on("submit", "#addComment", AddComment);
 })
 
 function AddAnswer(event) {
@@ -13,7 +14,24 @@ function AddAnswer(event) {
 	var formComment = $("#newComment").children(".form_comment");
 	var height = formComment.height();
 	formComment = formComment.clone(true, true);
+	formComment.find(".login-parent").children().val("");
 	formComment.css("height", 0);
 	$(this).after(formComment);
 	formComment.animate({"height": height + "px"});
+}
+
+function AddComment(event) {
+	event.preventDefault();
+	if ($(this).find("input[name=login]").val() == undefined || $(this).find("textarea").val() == undefined)
+		return false;
+
+	var comment = $("<div class='comment-parent clearfix'><section class='comment'><p><span class='login'></span><span class='date-time'></span></p><p class='text-of-comment'></p><a class='answer-link' id='addAnswer' href='#'>Ответить</a></section><section class='answers'></section></div>");
+	var answer = $(this).closest("section").attr("id") == "newComment" ? false : true;
+	comment.find("span.login").text($(this).find("input[name=login]").val());
+	comment.find(".text-of-comment").text($(this).find("textarea").val());
+	if (answer)
+		$(this).closest(".comment").next().prepend(comment);
+	else
+		$("#oldComments").prepend(comment);
+	$(this).parent().animate({"height": 0}, function () { $(this).remove(); });
 }
