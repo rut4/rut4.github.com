6330eeb5af5fb0b40c61b20301bc35aef645636c
Eduard <rut4ik@gmail.com>
2013-05-25 19:05:43 +0400

    date time

--- homework/hw_1/js/hw_1_1.js
+++ homework/hw_1/js/hw_1_1.js
@@ -5,33 +5,53 @@ $(function () {
 	}
 	$("#comments")
 	.on("click", "#addAnswer", AddAnswer)
-	.on("submit", "#addComment", AddComment);
+	.on("submit", "#addComment", AddComment)
+	.on("click", "#deleteAnswersForm", DeleteAnswersForm);
 })
 
 function AddAnswer(event) {
 	event.preventDefault();
-	$("#oldComments").find(".form_comment").animate({"height": 0}, function () { $(this).remove(); });
+	var oldComments = $("#oldComments");
+	oldComments.find(".form_comment").slideUp(500, function() { $(this).remove() });
+	oldComments.find("#deleteAnswersForm").text("Ответить").attr("id", "addAnswer");
 	var formComment = $("#newComment").children(".form_comment");
 	var height = formComment.height();
 	formComment = formComment.clone(true, true);
 	formComment.find(".login-parent").children().val("");
-	formComment.css("height", 0);
+	formComment.css("display", "none");
 	$(this).after(formComment);
-	formComment.animate({"height": height + "px"});
+	formComment.slideDown(500);
+	$(this).text("Отменить").attr("id", "deleteAnswersForm");
+}
+
+function DeleteAnswersForm (event) {
+	event.preventDefault();
+	$(this).next().slideUp(500, function() { $(this).remove() });
+	$(this).text("Ответить").attr("id", "addAnswer");
 }
 
 function AddComment(event) {
 	event.preventDefault();
-	if ($(this).find("input[name=login]").val() == "" || $(this).find("textarea").val() == "")
+	if ($(this).find("input[name=login]").val() == "" || $(this).find("textarea").val() == "") {
 		return false;
+	}
 
 	var comment = $("<div class='comment-parent clearfix'><section class='comment'><p><span class='login'></span><span class='date-time'></span></p><p class='text-of-comment'></p><a class='answer-link' id='addAnswer' href='#'>Ответить</a></section><section class='answers'></section></div>");
 	var answer = $(this).closest("section").attr("id") == "newComment" ? false : true;
 	comment.find("span.login").text($(this).find("input[name=login]").val());
 	comment.find(".text-of-comment").text($(this).find("textarea").val());
+
+	$.ajax("http://json-time.appspot.com/time.json?tz=Europe/Moscow", {
+		type: "get",
+		dataType: "json",
+		success: function (data) {
+			var day = data.datetime.slice(0, 3);
+			comment.find(".date-time").text(day);
+		}
+	});
 	if (answer) {
 		$(this).closest(".comment").next().prepend(comment);
-		$(this).parent().animate({"height": 0}, function () { $(this).remove(); });
+		$(this).parent().slideUp(500, function() { $(this).remove() });
 	}
 	else {
 		$("#oldComments").prepend(comment);
